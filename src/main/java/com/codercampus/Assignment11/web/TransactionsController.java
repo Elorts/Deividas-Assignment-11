70da1bc2d2dd7cc42ec6e7edf9ad68d221e338af
deividas strole <deiwis@yahoo.com>
2024-05-16 22:34:07 -0700

    created controller, service files and some output to html

--- src/main/java/com/codercampus/Assignment11/web/TransactionsController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionsController.java
@@ -1,15 +1,37 @@
 package com.codercampus.Assignment11.web;
 
+import com.codercampus.Assignment11.domain.Transaction;
+import com.codercampus.Assignment11.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-//
+
+import java.util.List;
+
 @Controller
 public class TransactionsController {
 
+    @Autowired
+    private TransactionService transactionService;
+
     @GetMapping("/transactions")
     public String getTransactions(ModelMap model) {
+
+        List<Transaction> transactions = transactionService.findAll();
+
+        model.put("transactions", transactions);
+
+
+//        // tc
+//        for (Transaction t : transactions) {
+//
+//
+//            //System.out.println(t.getRetailer());
+//        }
+//
+//        // /tc
+
         return "transactions";
     }
 }

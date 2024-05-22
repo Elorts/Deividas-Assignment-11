aac6a8c9bf974c41299b3fa281ae1c514c4051b7
deividas strole <deiwis@yahoo.com>
2024-05-21 22:15:32 -0700

    Working on html vars

--- src/main/java/com/codercampus/Assignment11/service/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/service/TransactionService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
@@ -13,10 +14,28 @@ public class TransactionService {
     @Autowired
     TransactionRepository transactionRepo;
 
+    
     public List<Transaction> findAll() {
 
-        List <Transaction> unsortedList = transactionRepo.findAll();
-        unsortedList.sort((e1, e2) -> e1.getDate().compareTo(e2.getDate()));
-        return unsortedList;
+        List<Transaction> listOfTransactions = getTransactions();
+        listOfTransactions.sort((e1, e2) -> e1.getDate().compareTo(e2.getDate()));
+        return listOfTransactions;
+    }
+
+    private List<Transaction> getTransactions() {
+        List <Transaction> listOfTransactions = transactionRepo.findAll();
+        return listOfTransactions;
+    }
+
+    public Transaction findById(Long id) {
+//        Optional<Transaction> resultById = listOfTransactions.stream().filter(item -> item.equals(id))
+//                                                                      .findFirst();
+        Transaction resultById = null;
+
+        for (Transaction t : getTransactions())
+            if (t.getId() == id)
+                resultById = t;
+        
+        return resultById;
     }
 }
